68ba6890c1d8e8171c7286f84888a663125cd283
lufimio <67525443+PA055@users.noreply.github.com>
2024-09-28 13:46:25 -0400

    feat: :sparkles: drag and drop :fire:

--- circuits.js
+++ circuits.js
@@ -1,2 +1,16 @@
-import interact from 'interactjs'
+position = {x: 0, y: 0};
 
+interact('.component').draggable({
+    listeners: {
+      start (event) {
+        console.log(event.type, event.target)
+      },
+      move (event) {
+        position.x += event.dx
+        position.y += event.dy
+  
+        event.target.style.transform =
+          `translate(${position.x}px, ${position.y}px)`
+      },
+    }
+  })
